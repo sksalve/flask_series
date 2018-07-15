b651281653752f748c13c9d7a90d78d7be598112
sksalve <35111033+sksalve@users.noreply.github.com>
2018-07-16 01:06:24 +0530

    new

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a20dbbd2cd97b23967bba7e46f91c60b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt()
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
+
